8393c23d4c292284e14ea3b7448075fa3e3514f1
wjdgkals23 <wjdgkals74@gmail.com>
2018-10-02 19:27:51 +0900

    First Commit  * Set front/back  * Set Total View Frame

---
+++ frontend/app/components/App.vue
@@ -0,0 +1,92 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <ActivityIndicator :busy=busy @busyChange="onBusyChanged" />
+        <GridLayout rows="*,60">
+            <ScrollView row="0">
+                <WrapLayout orientation="horizontal">
+                    <GridLayout v-for="item in data" width="50%" height="150" rows="2*,*">
+                        <Image
+                                :src="item.src"
+                                stretch="aspectFill"
+                                row="0"
+                                width="100%"
+                        ></Image>
+                        <Label
+                                row="1"
+                                :text=item.title
+                                horizontalAlignment="center"
+                                verticalAlignment="bottom"
+                                backgroundColor="rgb(81,197,247, 0.8)"
+                                padding="10"
+                                fontSize="13"
+                                color="white"
+                        ></Label>
+                    </GridLayout>
+                </WrapLayout>
+            </ScrollView>
+            <StackLayout orientation="horizontal" row="1" id="navigation">
+                <Label width="25%" class="fa" :text="'fa-eye' | fonticon" @tap="talk"/>
+                <Label width="25%" class="fa" :text="'fa-heart' | fonticon" />
+                <Label width="25%" class="fa" :text="'fa-user' | fonticon" />
+                <Label width="25%" class="fa" :text="'fa-user' | fonticon" />
+            </StackLayout>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'Hello World!',
+                data:[
+                    {title:"head", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"body", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"leg", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"foot", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"head", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"body", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"leg", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"foot", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"head", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"body", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"leg", src: "~/assets/images/NativeScript-Vue.png"},
+                    {title:"foot", src: "~/assets/images/NativeScript-Vue.png"}
+                ],
+                busy: true
+            }
+        },
+        methods:{
+            talk: function(){
+                console.log("talk");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20px;
+        color: #333333;
+    }
+
+    #navigation {
+        background-color: aquamarine;
+    }
+
+    #navigation Label {
+        text-align:center;
+        font-size: 25%;
+        color: darkslategray;
+        vertical-align: middle;
+        /*hover*/
+    }
+</style>
---
+++ frontend/app/main.js
@@ -0,0 +1,24 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+import VueDevtools from 'nativescript-vue-devtools'
+import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
+
+TNSFontIcon.debug = true;
+TNSFontIcon.paths = {
+    'fa': './assets/css/font-awesome.min.css',
+    'ion': './assets/css/ionicons.min.css'
+};
+TNSFontIcon.loadCss();
+
+Vue.filter('fonticon', fonticon);
+
+if(TNS_ENV !== 'production') {
+    Vue.use(VueDevtools)
+}
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+
+new Vue({
+    render: h => h('frame', [h(App)])
+}).$start()
