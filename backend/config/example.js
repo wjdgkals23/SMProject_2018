99d9f7a6d07e214366a90426cb2946701f563d09
wjdgkals23 <wjdgkals74@gmail.com>
2018-10-03 23:54:22 +0900

    Set DB connect  * set mysql connect

---
+++ backend/config/example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: "localhost",
+    port: "3306",
+    user: 'root',
+    password: 'your_password',
+    database: 'your_schema',
+    multipleStatements: true
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var db = require("../config/example");
+var conn = mysql.createConnection(db);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var sql = "SELECT * FROM TEST";
+  conn.query(sql, function(err, results){
+    if(err){
+        console.log(err);
+    }
+    else{
+      console.log(results);
+    }
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
